b61a408ffa0a58335525a5d6772348fd15c4f0a8
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-03-21 23:41:34 -0400

    (randomized) situation table implemented, and updated readme

--- README.md
+++ README.md
@@ -2,16 +2,36 @@
 first python project: simple genetic algorithm
 
 
-###todo: (See book, type these up in more detail when not so tired)
-*fix what is there so it is more in line with what it shoudl be
-*create situation table (ie, what to do depending on n/s/e/w/c status) [seperate file, import or load or something]
-*will need a view object or function or something for above, to use current space to look up strategy for that type of situation (243 total combinations (some not possible but whatever) therefore 243 possible reactions in each robot's strategy)
-*method for robot to perform its search (performs 200 actions)
-*run search for 200 originals
-*give each fitness rating(how to score them?!?!)
-*apply evolution(see book for details)
-*repeat for 1000 generations and compare original ratings with 1000th decendents ratings
-*eventually add way to interact with the program to make own adjustments to numbers, etc.
-*add lots of statistics/info printouts
-*add graphics capability eventually?  play 1 random original vs 1 random 1000th child for animated comparison (at least use curses?)
-*after learning more python web stuff give it a front end and put it online?
+###next todo:
+
+* next implement function/method to actually have a robot follow the strategy laid out in the table (must detect items, wall collisions, etc)  (for now have it perform 200 actions)
+
+* will need a view object or function or something for above, to tell state of current neighborhood -- it gets passed as key to table to lookup action
+
+* calculate fitness rating (how to score them?!?! she doesn't mention how she does this in the book(?) so I will have to figure out my own...)
+
+
+###and then, moving on:
+
+* run search for 200 originals
+
+* apply evolution using fitness ratings(see book for details)
+
+* repeat this for 1000 generations and compare original ratings with 1000th decendents ratings
+
+
+###and then, eventually....
+
+* add way to interact with the program to make own adjustments (number of generations, number of actions per robot, etc)
+
+* add lots of statistics/info printouts
+
+* add graphics capability eventually?  play 1 random original vs 1 random 1000th child for animated comparison (at least use curses for SOME visual?)
+
+* maybe after learning more python web stuff make it more of a web app and give it a simple html/css/js UI
+
+
+###also:
+
+* make comments more thorough and CLEAR!
+
--- ga.py
+++ ga.py
@@ -4,25 +4,55 @@ import random
 class Robot:
 
     def __init__(self):
-        '''initialize each robot with randomized list of instructions (243 for now) 0-north, 1-south, 2-east,3-west,4-stay,5-pick up,6-random move'''
+        '''initialize each robot with randomized list of instructions
+        (162 but some are not actually possible)
+         0-north, 1-south, 2-east,3-west,4-stay,5-pick up,6-random move'''
 
         self.genes = []
-        for x in range(243):
-            self.genes.append(random.randrange(0, 7))
+        for x in range(162):
+            self.genes.append(random.randrange(7))
 
     def __str__(self):
             return ''.join(map(str, self.genes))
 
 
+class Situation_Table:
+
+    def __init__(self):
+        '''just initialize with *randomized* actions rather than
+        entering 162 potential situations by hand and see how it goes...
+        encoding: each current or adjacent space has 3 possible states:
+        is empty, has item, or is a wall....make each possible combination
+        a tuple of five numbers (0 empty, 1 item, 2 wall) in n/e/w/s/c order
+        (ie north, east, west, south, and current) -- and use as key in dict
+        to lookup action to take (0-6) see above'''
+
+        self.table = {}
+
+        for n in range(3):
+            for e in range(3):
+                for w in range(3):
+                    for s in range(3):
+                        for c in range(2):  #can't BE on a wall
+                            self.table[(n, e, w, s, c)] = random.randrange(7)
+
+    def __str__(self):
+        output = []
+        for k in self.table.keys():
+            output.append('situation: ' + str(k) + '  action: ' + str(self.table[k]))
+        return '\n'.join(output)
+
+
 class Grid:
 
     def __init__(self):
-        '''create 10x10 grid with 50 percent chance of any cell containing an item for the robot to pick up'''
+        '''create 10x10 grid with 50 percent chance of any
+        cell containing an item for the robot to pick up'''
 
         self.grid = {}
         for y in range(10):
             for x in range(10):
-                if random.randrange(0, 2) == 1:
+                if random.randrange(2) == 1:
                     self.grid[(x, y)] = True
                 else:
                     self.grid[(x, y)] = False
@@ -37,6 +67,22 @@ class Grid:
                 msg = ' is empty'
             msgs.append("cell " + str(k) + msg)
         return '\n'.join(msgs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def main():
@@ -54,6 +100,9 @@ def main():
 
     for x in range(5):  #just print first five original robots as a test/example
         print ("Robot " + str(x) + ": " + str(robots[x]))
+
+    table = Situation_Table()
+    print (table)
 
 
 main()
