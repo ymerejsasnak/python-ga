b5f7cede58d85a84bdf68b73f6b94ef29f76e03f
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-03-23 13:27:08 -0400

    move method works and fixed 'situation table' so its just part of each robot

--- ga.py
+++ ga.py
@@ -6,47 +6,59 @@ class Robot:
     def __init__(self):
         '''initialize each robot with position and randomized instructions
         (162 but some are not actually possible)
-         0-north, 1-south, 2-east,3-west,4-stay,5-pick up,6-random move'''
-        #will need to adjust this for children robots eventually
-        self.x = 0
-        self.y = 0
-        self.genes = []
-        for x in range(162):
-            self.genes.append(random.randrange(7))
-
-    def __str__(self):
-            return ''.join(map(str, self.genes))
-
-    def move(self, grid):
-        situation = grid.get_situation(self.x, self.y)
-        #use situation variable to lookup action, then take action (checking for wall collisions since sit table is random)
-
-
-class Situation_Table:
-
-    def __init__(self):
-        '''just initialize with *randomized* actions rather than
-        entering 162 potential situations by hand and see how it goes...
+        0-north, 1-south, 2-east,3-west,4-pick up,5-random move
         encoding: each current or adjacent space has 3 possible states:
         is empty, has item, or is a wall....make each possible combination
         a tuple of five numbers (0 empty, 1 item, 2 wall) in n/e/w/s/c order
         (ie north, east, west, south, and current) -- and use as key in dict
-        to lookup action to take (0-6) see above'''
-
-        self.table = {}
-
+        to lookup action to take (0-5) '''
+        #will need to adjust this for children robots eventually
+        self.x = 0
+        self.y = 0
+        self.score = 0
+        
+        self.genes = {}
         for n in range(3):
             for e in range(3):
                 for w in range(3):
                     for s in range(3):
                         for c in range(2):  #can't BE on a wall
-                            self.table[(n, e, w, s, c)] = random.randrange(7)
+                            self.genes[(n, e, w, s, c)] = random.randrange(6)
 
     def __str__(self):
         output = []
-        for k in self.table.keys():
-            output.append('situation: ' + str(k) + '  action: ' + str(self.table[k]))
+        for k in self.genes.keys():
+            output.append('situation: ' + str(k) + '  action: ' + str(self.genes[k]))
         return '\n'.join(output)
+
+    
+    def move(self, grid):
+        #use situation variable to lookup action, then take action (checking for wall collisions since sit table is random)
+        situation = grid.get_situation(self.x, self.y)
+        action = self.genes[situation]
+        if action == 0 and self.y > 0:
+            self.y -= 1
+        if action == 1 and self.y < 9:
+            self.y += 1
+        if action == 2 and self.x < 9:
+            self.x += 1
+        if action == 3 and self.x > 0:
+            self.x -= 1
+        if action == 4 and situation[4] == True:
+            self.score += 1 #just 1 point for each for now until i figure something better
+            grid.grid[(self.x, self.y)] = False #no more item in this spot
+        if action == 5: #not quite random as is right now
+            if random.randrange(2) == 0:
+                if random.randrange(2) == 0 and self.y > 0:
+                    self.y -= 1
+                elif self.y < 9:
+                    self.y += 1
+            else:
+                if random.randrange(2) == 0 and self.x > 0:
+                    self.x -= 0
+                elif self.x < 9:
+                    self.x += 0
+        return action
 
 
 class Grid:
@@ -76,24 +88,24 @@ class Grid:
 
     def get_situation(self, x, y):
         #remember: 0 empty, 1 item, 2 wall  in n/e/w/s/c order
-        current = (x, y)
         view = [] 
         if y == 0:
-            view[0] = 2
+            view.append(2)
         else:
-            view[0] = self.grid[(x, y - 1)]
+            view.append(self.grid[(x, y - 1)])
         if x == 9:
-            view[1] = 2
+            view.append(2)
         else:
-            view[1] = self.grid[(x + 1, y)]
+            view.append(self.grid[(x + 1, y)])
         if x == 0:
-            view[2] = 2
+            view.append(2)
         else:
-            view[2] = self.grid[(x - 1, y)]
+            view.append(self.grid[(x - 1, y)])
         if y == 9:
-            view[3] = 2
+            view.append(2)
         else:
-            view[3] = self.grid[(x, y + 1)]
+            view.append(self.grid[(x, y + 1)])
+        view.append(self.grid[(x, y)])
         return tuple(view)
         
 
@@ -120,8 +132,8 @@ def main():
     #below isn't quite right yet...getting ahead of myself...need to make it all work for 1 robot before I go further
 
     #create grid
-    g = Grid()
-    print (g)
+    grid = Grid()
+    print (grid)
 
     #create 200 randomized robots to start with
     robots = []
@@ -131,8 +143,11 @@ def main():
     for x in range(5):  #just print first five original robots as a test/example
         print ("Robot " + str(x) + ": " + str(robots[x]))
 
-    table = Situation_Table()
-    print (table)
+    for x in range(200):
+        print (robots[0].x, robots[0].y)
+        print (robots[0].move(grid))#just test first robot for now
+    print (robots[0].score)
+
 
 
 main()
