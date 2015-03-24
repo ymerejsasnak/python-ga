2e5d43cbfd144d28f25e40a36c416d77eed98ccd
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-03-24 10:23:48 -0400

    fixing a number of issues (str display, formatting, situation table)

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ first python project: simple genetic algorithm
 
 ###next todo:
 
+* NOTE: first will have to figure out a way to split up tables to they can be combined into childrens' tables (first idea that comes to mind is each situation tuple key (n,e,w,s,c) actually gets a value of an index (0 - 162 or whatever it was) and then have the actual moves for each robot be a list of 162 numbers (0 - 5 or hwatever) and then use the situation as key to lookup the index to then use to lookup the move...seems convoluted, a better way...????)
+
 * apply evolution using fitness ratings(see book for details)
 
 * repeat this for 1000 generations and compare original ratings (say avg and max?) with 1000th decendents ratings (avg and max)
--- ga.py
+++ ga.py
@@ -4,39 +4,36 @@ import random
 class Robot:
 
     def __init__(self):
-        '''initialize each robot with position and randomized instructions
-        (162 but some are not actually possible)
-        0-north, 1-south, 2-east,3-west,4-pick up,5-random move
-        encoding: each current or adjacent space has 3 possible states:
-        is empty, has item, or is a wall....make each possible combination
-        a tuple of five numbers (0 empty, 1 item, 2 wall) in n/e/w/s/c order
-        (ie north, east, west, south, and current) -- and use as key in dict
-        to lookup action to take (0-5) '''
-        #will need to adjust this for children robots eventually
-        self.x = 0
-        self.y = 0
+        #initialize each robot with position and randomized instructions
+        #(162 but some are not actually possible)
+        #0-north, 1-south, 2-east,3-west,4-pick up,5-random move
+        #encoding: each current or adjacent space has 3 possible states:
+        #is empty, has item, or is a wall....make each possible combination
+        #a tuple of five numbers (0 empty, 1 item, 2 wall) in n/e/w/s/c order
+        #(ie north, east, west, south, and current) -- and use as key in dict
+        #to lookup action to take (0-5)
+
+        #start robot roughly in the middle instead of 0,0 (does it matter?)
+        self.x = 4
+        self.y = 5
         self.score = 0
-        
-        self.genes = {}
-        for n in range(3):
-            for e in range(3):
-                for w in range(3):
-                    for s in range(3):
-                        for c in range(2):  #can't BE on a wall
-                            self.genes[(n, e, w, s, c)] = random.randrange(6)
+
+        self.genes = []
+        for g in range(162):
+            self.genes.append(random.randrange(6))
 
     def __str__(self):
-        output = []
-        for k in self.genes.keys():
-            output.append('situation: ' + str(k) + '  action: ' + str(self.genes[k]))
-        return '\n'.join(output)
+        return " ".join(map(str, self.genes)) + '\n'
 
-    
-    def move(self, grid):
-        #use situation variable to lookup action, then take action (checking for wall collisions)
-        #hit wall -5 points, pick up item +10 points, try to pick up but nothing there -1 points
+    def move(self, grid, table):
+        #use situation variable to lookup action,
+        #then take action (checking for wall collisions)
+        #hit wall -5 points, pick up item +10 points,
+        #try to pick up but nothing there -1 points
+
         situation = grid.get_situation(self.x, self.y)
-        action = self.genes[situation]
+        action = self.genes[table.lookup[situation]]
+
         #moves
         if action == 0:
             if self.y > 0:
@@ -60,14 +57,16 @@ class Robot:
                 self.score -= 10
         #pickup
         if action == 4:
-            if situation[4] == True:
-                self.score += 10 #just 1 point for each for now until i figure something better
-                grid.grid[(self.x, self.y)] = False #no more item in this spot
+            if situation[4]:
+                self.score += 10
+                #and remove item from spot:
+                grid.grid[(self.x, self.y)] = False
             else:
                 self.score -= 1
 
-        #final random action (rewrite it better when clearer headed...not quite random as is)
-        if action == 5: 
+        #final random action
+        #(rewrite it better when clearer headed...not quite random as is)
+        if action == 5:
             if random.randrange(2) == 0:
                 if random.randrange(2) == 0 and self.y > 0:
                     self.y -= 1
@@ -81,11 +80,29 @@ class Robot:
         return action
 
 
+class Situation_Table:
+
+    #give each unique situation its own numeric value which
+    #corresponds to the an index in the robot's action list (genes)
+    #c (current) is only 1 or 2 since robot can't BE on a wall
+
+    def __init__(self):
+        self.lookup = {}
+        index = 0
+        for n in range(3):
+            for e in range(3):
+                for w in range(3):
+                    for s in range(3):
+                        for c in range(2):
+                            self.lookup[(n, e, w, s, c)] = index
+                            index += 1
+
+
 class Grid:
 
     def __init__(self):
-        '''create 10x10 grid with 50 percent chance of any
-        cell containing an item for the robot to pick up'''
+        #create 10x10 grid with 50 percent chance of any
+        #cell containing an item for the robot to pick up'''
 
         self.grid = {}
         for y in range(10):
@@ -97,18 +114,17 @@ class Grid:
 
     def __str__(self):
         msgs = []
-        msg = ''
-        for k in self.grid.keys():
-            if self.grid[k]:
-                msg = ' has an item'
-            else:
-                msg = ' is empty'
-            msgs.append("cell " + str(k) + msg)
-        return '\n'.join(msgs)
+        for y in range(10):
+            msg = ''
+            for x in range(10):
+                msg += str(int(self.grid[(x, y)])) + " "
+            msgs.append(msg)
+
+        return '\n'.join(msgs) + '\n'
 
     def get_situation(self, x, y):
         #remember: 0 empty, 1 item, 2 wall  in n/e/w/s/c order
-        view = [] 
+        view = []
         if y == 0:
             view.append(2)
         else:
@@ -127,55 +143,26 @@ class Grid:
             view.append(self.grid[(x, y + 1)])
         view.append(self.grid[(x, y)])
         return tuple(view)
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 def main():
 
-    #below is a mess for now...testing stuff, etc
-
     #create grid
     grid = Grid()
-    print (grid)
-
+    #create lookup table
+    table = Situation_Table()
     #create 200 randomized robots to start with
     robots = []
     for x in range(200):
         robots.append(Robot())
 
-
-
-
-    #for x in range(5):  #just print first five original robots as a test/example
-    #    print ("Robot " + str(x) + ": " + str(robots[x]))
-
     #each robot
     for r in robots:
         #moves 200 times
         for i in range(200):
-            r.move(grid)
+            r.move(grid, table)
         #then prints score
         print (r.score)
-
-
-
 
 
 main()
